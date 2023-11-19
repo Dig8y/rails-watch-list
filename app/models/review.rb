3a918a605a6d194057ab0a80e784e20c962be47a
Digby Hebb <digby.hebb@gmail.com>
2023-11-19 22:08:21 +0000

    fixed review validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :list
-  validates :rating, :comment, presence: true
-  validates :comment, length: { minimum: 6}
   validates :rating, numericality: { only_integer: true }
   validates :rating, numericality: { in: 0..5 }
 end
